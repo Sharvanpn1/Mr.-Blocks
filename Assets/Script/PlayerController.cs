548bffbb75d5270f2f18bd2d96de5b0305f71c88
Sharvan Poonia <sharvanpn@gmail.com>
2022-07-05 13:09:13 +0530

    Initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D RigidBody2D;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            RigidBody2D.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            RigidBody2D.velocity = new Vector2(-speed, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            RigidBody2D.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            RigidBody2D.velocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            RigidBody2D.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Door")
+            Debug.Log("Level Coimplete");
+
+    }
+}
