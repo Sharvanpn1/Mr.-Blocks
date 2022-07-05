f2311b7d960ad3537e8f70e7e17e1a86091bab59
Sharvan Poonia <sharvanpn@gmail.com>
2022-07-05 14:38:59 +0530

    Adding Game Pause/Game Won Screens

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D RigidBody2D;
 
+    public GameObject GameWonCanvas;
+
+    public GameObject GamePauseCanvas;
+
+    private bool isGameWon = false;
+
+    private bool isGamePaused = false;
+
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +24,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isGameWon || isGamePaused)
+        {
+            RigidBody2D.velocity = new Vector2(0f, 0f);
+            return;
+        }
+        if (Input.GetAxis("Cancel") > 0)
+        {
+            RigidBody2D.velocity = new Vector2(0f, 0f);
+            GamePauseCanvas.SetActive(true);
+            isGamePaused = true;
+            return;
+        }
         if (Input.GetAxis("Jump") > 0)
         {
             RigidBody2D.velocity = new Vector2(0f, 0f);
@@ -40,12 +60,18 @@ public class PlayerController : MonoBehaviour
         {
             RigidBody2D.velocity = new Vector2(0f, 0f);
         }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Door")
+        {
             Debug.Log("Level Coimplete");
+            GameWonCanvas.SetActive(true);
+            isGameWon = true;
+        }
+
 
     }
 }
